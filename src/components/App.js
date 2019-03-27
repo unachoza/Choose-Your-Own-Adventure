5bf3fe350380eeb28d257e4babc4a1f3ddf269c3
Arianna Choza <unachoza@gmail.com>
2019-03-27 07:45:38 -0400

    react boiler plate

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component{
+    render() {
+        return (
+            <div>Choose your own adventure!</div>
+        )
+    }
+}
+
+export default App
